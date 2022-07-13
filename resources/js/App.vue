9f478c726c8ec8a5d3702f8caf382e6688f1f90f
GioCri01 <giovanni.crispino94@icloud.com>
2022-07-13 17:28:38 +0200

    innestato APP.vue in vue e stampato i post in pagina

---
+++ resources/js/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div >
+    <div class="container">
+
+        <ul>
+            <li
+            v-for="post in posts"
+            :key="post.id"
+            >
+                <h3>{{ post.title }}</h3>
+
+                <p>{{ post.content }}</p>
+            </li>
+        </ul>
+
+    </div>
+
+
+
+  </div>
+</template>
+
+<script>
+import Axios from 'axios';
+
+export default {
+    name: "App",
+
+    data(){
+        return{
+            apiUrl : "http://127.0.0.1:8000/api/posts",
+            posts : [],
+        }
+    },
+
+    mounted(){
+        this.getApi();
+    },
+
+    methods:{
+        getApi(){
+            axios.get(this.apiUrl)
+            .then(res =>{
+                this.posts = res.data;
+                console.log(this.posts);
+            })
+        }
+    }
+
+
+}
+</script>
+
+<style>
+
+</style>
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -5,9 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset("css/front/style.css") }}">
+
     <title>Document</title>
 </head>
 <body>
-HOME
+<div id="app"></div>
+
+
+
+
+<script src="{{ asset('js/front.js') }}"></script>
 </body>
 </html>
